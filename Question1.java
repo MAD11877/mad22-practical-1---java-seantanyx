926582bd2740ba132f3dbc7819e9f2bc4f6f5673
seantanyx <93499896+seantanyx@users.noreply.github.com>
2022-04-20 17:24:35 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -2,10 +2,9 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    /**
-     * Print "Hello World!"
-     * 
-     * Hint: System.out.println(...)
-     */
+    
+      System.out.println("hello world");
+      
+     
   }
 }
