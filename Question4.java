18bec4bfb8a8aea16ccdc9aea131213e796e9dfe
seantanyx <93499896+seantanyx@users.noreply.github.com>
2022-04-21 10:16:42 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -18,6 +18,12 @@ public class Question4
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int rows=5;
+    for (int i= rows-1; i>=0 ; i--)  
+    {  
+    for (int j=0; j<=i; j++)  
+    System.out.print("*" + " ");  
+    System.out.println();  
+    }
   }
 }
