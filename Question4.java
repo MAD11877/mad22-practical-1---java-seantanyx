dc430659e89f0d60b4c0485ab96f7015853fea4e
seantanyx <93499896+seantanyx@users.noreply.github.com>
2022-04-21 12:47:50 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -22,7 +22,7 @@ public class Question4
     for (int i= rows-1; i>=0 ; i--)  
     {  
     for (int j=0; j<=i; j++)  
-    System.out.print("*" + " ");  
+    System.out.print("*");  
     System.out.println();  
     }
   }
